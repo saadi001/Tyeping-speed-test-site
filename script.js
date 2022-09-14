5dec2521d1386832c3245a028b9706d27b203934
Sheikh sadi <you@example.com>
2022-09-15 03:19:27 +0600

    blog html file created

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ const start = () => {
   // If already started, do not start again
   if (startTime) return;
 
-  let count = 1;
+  let count = 3;
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
